5d43ea8a1772ef7de1d655b0cd7728b65b95cc40
Guesmi Zaineb <132718792+guzaineb@users.noreply.github.com>
2025-03-03 04:11:13 +0100

    gestion eventement

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "*.{html, js}",
+  ],
+  theme: {
+   
+        extend: {
+            colors: {
+                cream: '#f8e6ce',
+                darkBrown: '#423c31',
+                burgundy: '#5f1d1d',
+                lightCream: '#e3e1ba',
+                mediumBrown: '#ad9a84',
+                copper: '#934c16',
+                brightRed: '#bd3030',
+                purple: '#6750a4',
+                green: '#14ae5c'
+            }
+        }
+    ,
+  },
+  plugins: [],
+}
+
